17fab89b9efcd9d7f0868fb62f9fec03e91e23c9
Edgar1092 <edgaralexander1008@gmail.com>
2020-07-22 16:30:06 -0400

    cambios

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://xionis.envioseurocarga.com/backend/public/api',
+  apiUrl: 'https://xionis.envioseurocarga.com/backend/public/index.php/api',
   apiBase: 'https://xionis.envioseurocarga.com/backend',
   serviceWorker: false
 };
